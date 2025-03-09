bf7745398e806b1c24241fe464f78a5effd62080
rashed-zaman <bangla.bd.rashed@gmail.com>
2025-03-09 12:54:55 +0600

    Update api.php

--- api.php
+++ api.php
@@ -11,18 +11,19 @@ use App\Http\Controllers\UserController;
 
 
 Route::post('/post', [PostController::class, 'PostCreate'])->name('PostCreate');
-Route::get('/list', [PostController::class, 'PostList'])->name('PostList');
-Route::get('/list-by-id', [PostController::class, 'PostByID'])->name('PostByID');
+Route::get('/post-list', [PostController::class, 'PostList'])->name('PostList');
+Route::get('/list-by-id/{id}', [PostController::class, 'PostByID'])->name('PostByID');
 
 
 
 Route::post('/user-registration', [UserController::class, 'UserRegistration'])->name('UserRegistration');
-Route::get('/user-by-id', [UserController::class, 'UserByID'])->name('UserByID');
+Route::get('/user-by-id/{id}', [UserController::class, 'UserByID'])->name('UserByID');
 
 
 Route::post('/task', [TaskController::class, 'TaskCreate'])->name('TaskCreate');
 Route::get('/task-list', [TaskController::class, 'TaskList'])->name('TaskList');
-Route::post('/task-update', [TaskController::class, 'TaskUpdate'])->name('TaskUpdate');
+Route::post('/task-update/{id}', [TaskController::class, 'TaskUpdate'])->name('TaskUpdate');
+
 
 
 
